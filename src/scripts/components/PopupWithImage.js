292e014ecc0f6c34a78d2fec1726891f1b15b206
Dmitry Neklyudov <dima.kazanec.394@yandex.ru>
2020-08-31 02:16:38 +0300

    промежуточный

--- src/scripts/components/PopupWithImage.js
+++ src/scripts/components/PopupWithImage.js
@@ -1,14 +1,35 @@
 import Popup from './Popup.js';
-export default class extends Popup {
+
+/**
+ * @description Класс PopupWithImage<br>
+ * Отвечает за открытие и закрытие попапа с полноразмерным изображением
+ * @param {String} popupSelector - селектор попапа
+ * @param {String} closeIconSelector - селектор иконки закрытия попапа
+ * @param {String} isOpenedModifier - модификатор открытого состояния попапа
+ * @param {String} cardImageSelector - селектор изображения карточки
+ * @param {String} cardTitleSelector - селектор элемента названия карточки
+ */
+export default class PopupWithImage extends Popup {
   constructor(popupSelector, closeIconSelector, isOpenedModifier, cardImageSelector, cardTitleSelector) {
     super(popupSelector, closeIconSelector, isOpenedModifier);
     this._cardImageElement = document.querySelector(cardImageSelector);
     this._cardTitleElement = document.querySelector(cardTitleSelector);
   }
 
+  /**
+   * @description Публичный метод<br>
+   * Открывает попап с полноразмерным изображением<br>
+   * Метод принимает аргументом объект с данными изображения карточки
+   * @param {Object} Object - объект с данными изображения карточки
+   * @param {String} Object.title - название карточки
+   * @param {String} Object.link - ссылка на изображение
+   */
   open({ title, link }) {
+    //Вызываем родительский метод
     super.open();
+    //Присваиваем полученную ссылку элемнту изображения попапа
     this._cardImageElement.src = link;
+    //Присваиваем полученное название элементу названия попапа
     this._cardTitleElement.textContent = title;
   }
 }
